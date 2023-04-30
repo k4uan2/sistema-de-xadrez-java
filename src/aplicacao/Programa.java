076b3f5ae068161390839bcb99e42ac42baa047b
Kauan Victor <k4uan222@gmail.com>
2023-04-30 13:06:03 -0300

    Camada de xadrez e printando o tabuleiro

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -2,11 +2,15 @@ package aplicacao;
 
 import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
+import xadrez.PartidaDeXadrez;
+import xadrez.PartidaDeXadrez;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+		
+		PartidaDeXadrez partidaDexadrez = new PartidaDeXadrez();
+		UI.printTabuleiro(partidaDexadrez.getPecas());
 	}
 
 }
